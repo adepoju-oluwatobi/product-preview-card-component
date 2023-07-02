4557c9ba715487fe150ed3398d0949c595f9d4f3
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-02 17:34:33 +0100

    finished

--- src/components/ProductPreview.jsx
+++ src/components/ProductPreview.jsx
@@ -5,7 +5,7 @@ import Cart from '../assets/icon-cart.svg'
 
 function ProductPreview() {
   return (
-    <div className='bg-white w-[90%] h-[70%] m-auto rounded-2xl md:flex '>
+    <div className='bg-white w-[90%] md:w-[800px] h-[70%] md:h-[600px] m-auto rounded-2xl md:flex '>
        <div className='md:w-[50%]'>
         <img className='md:hidden rounded-t-2xl' src={producImgMobile} alt="" />
         <img className='hidden rounded-l-2xl md:block' src={producImgDesktop} alt="" />
@@ -21,7 +21,7 @@ function ProductPreview() {
         </p>
         <div className='flex items-center gap-2'>
             <p className='product-price'>$149.99</p>
-            <s className='opacity-50 text-xs'>$169.99</s>
+            <p><s className='opacity-50 text-black md:text-sm'>$169.99</s></p>
         </div>
         <button className='btn'>
             <img className='w-3 md:w-5' src={Cart} alt="" />
